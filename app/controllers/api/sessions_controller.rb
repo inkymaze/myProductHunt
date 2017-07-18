3c90d599e13c54702eb31814dc73455cf364944e
inkymaze <nickwcannon@gmail.com>
2017-07-18 11:14:37 -0700

    add backend auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+    params[:user][:username],
+    params[:user][:password])
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      sign_out
+      render "api/users/show"
+    else
+      render(json: ["Nobody signed in"], status: 404)
+    end
+  end
+end
