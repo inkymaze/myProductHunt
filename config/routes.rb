0638c5d1bc7e49deb740d7d80ead57a92d754882
inkymaze <nickwcannon@gmail.com>
2017-07-18 11:41:45 -0700

    add user views and session util files

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'sessions/new'
+
+  namespace :api, defaults: { format: :json } do
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
   end
 
-  namespace :api do
-    get 'sessions/create'
-  end
-
-  namespace :api do
-    get 'sessions/destroy'
-  end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
