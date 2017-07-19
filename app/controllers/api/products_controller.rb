2dae49aeabeec6dfe3ddbf4184e24bc3ab0d41f3
inkymaze <nickwcannon@gmail.com>
2017-07-19 16:51:59 -0700

    product controller and components

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,10 +1,29 @@
 class Api::ProductsController < ApplicationController
+
+  before_action :require_logged_in, only: [:create]
+  
   def create
+    @product = Product.new(product_params)
+    @product.hunter_id = current_user.id
+    if @product.save
+      render "api/products/show"
+    else
+      render json: @product.errors.full_messages, status: 422
+    end
   end
 
   def show
+    @product = Product.find_by(id: params[:id])
   end
 
   def index
+    @products = Product.all
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :desc, :image_url, :product_url)
   end
 end
