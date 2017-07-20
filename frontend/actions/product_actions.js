d220bb24471d5031dbb427d0e00f9e106edbb621
inkymaze <nickwcannon@gmail.com>
2017-07-20 15:20:35 -0700

    build product actions and product reducer frame

---
+++ frontend/actions/product_actions.js
@@ -0,0 +1,26 @@
+import * as APIUtil from '../util/product_api_util';
+
+export const RECEIVE_PRODUCTS = "RECEIVE_PRODUCTS";
+export const RECEIVE_PRODUCT = "RECEIVE_PRODUCT";
+export const RECEIVE_PRODUCT_ERRORS = "RECEIVE_PRODUCT_ERRORS";
+export const CREATE_PRODUCT = "CREATE_PRODUCT"
+
+export const receiveProducts = (products) => ({
+  type: RECEIVE_PRODUCTS,
+  products
+});
+
+export const receiveProduct = (product) => ({
+  type: RECEIVE_PRODUCT,
+  product
+});
+
+export const createProduct = (product) => ({
+  type: CREATE_PRODUCT,
+  product
+});
+
+export const receiveProductErrors = (errors) => ({
+  type: RECEIVE_PRODUCT_ERRORS,
+  errors
+});
---
+++ frontend/reducers/product_reducer.js
@@ -0,0 +1,15 @@
+import merge from 'lodash/merge';
+
+import {  RECEIVE_PRODUCTS,
+          RECEIVE_PRODUCT,
+          RECEIVE_PRODUCT_ERRORS,
+       } from '../actions/product_actions';
+
+const _defaultState ={
+
+
+};
+
+const ProductReducer = (state = _defaultState, action) => {
+
+};
