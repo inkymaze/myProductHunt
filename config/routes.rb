2001beb98861005ab256ce9eae04e8a78eceb7ba
inkymaze <nickwcannon@gmail.com>
2017-07-20 09:31:32 -0700

    add product routes to api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    get 'products/create'
-  end
-
-  namespace :api do
-    get 'products/show'
-  end
-
-  namespace :api do
-    get 'products/index'
-  end
-
   namespace :api, defaults: { format: :json } do
     resource :user, only: [:create]
+    resources :products, only: [:create, :show, :index]
     resource :session, only: [:create, :destroy, :show]
   end
 
