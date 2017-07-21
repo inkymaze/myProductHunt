a9681714e69bc6310b131553c968a8c29a0c1950
inkymaze <nickwcannon@gmail.com>
2017-07-20 20:23:34 -0700

    fix typo

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -26,8 +26,8 @@ const App = () => (
     <span className="intro-span">
     <div className="intro-greeting">
       <h1>Discover your next favorite thing</h1>
-      <h6>Pinpoint surfaces the best new products, every day.
-        Its a place for product-loving enthusiasts to share and
+      <h6>Product Hunt surfaces the best new products, every day.
+        It's a place for product-loving enthusiasts to share and
         geek out about the latest mobile apps, websites,
         hardware projects, and tech creations</h6>
       <Link className="intro-signup" to="/signup">SIGN UP</Link>
