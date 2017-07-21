4d426779585e7ca2056b2226b2687042a8023744
inkymaze <nickwcannon@gmail.com>
2017-07-21 10:04:53 -0700

    begin product reducer and form

---
+++ frontend/components/product/add_product_form.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class AddProductForm {
+  constructor(props) {
+    super(props);
+    this.state = {
+     name: "",
+     image_url: "",
+     product_url: "",
+     desc: "",
+     hunter_id: ""
+   };
+  }
+}
+
+
+export default AddProductForm;
--- frontend/reducers/product_reducer.js
+++ frontend/reducers/product_reducer.js
@@ -6,9 +6,29 @@ import {  RECEIVE_PRODUCTS,
        } from '../actions/product_actions';
 
 const _defaultState ={
-
+  byId: {},
+  allIds: [],
+  errors: null
 };
 
 const ProductReducer = (state = _defaultState, action) => {
+  Object.freeze(state);
 
+
+  switch(action.type) {
+    case RECEIVE_PRODUCTS:
+
+
+    case RECEIVE_PRODUCT:
+
+    case RECEIVE_PRODUCT_ERRORS:
+
+
+    default:
+      return state;
+  }
 };
+
+
+
+export default ProductReducer;
