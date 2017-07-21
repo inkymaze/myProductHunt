83381971828967d1a7e3cbda18632124278032f0
inkymaze <nickwcannon@gmail.com>
2017-07-20 17:43:23 -0700

    add intro,kitty pic, addition sign up button

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -23,12 +23,24 @@ const App = () => (
       <input type="text"  class="search-bar" placeholder="Discover your next favorite thing..."></input>
       <GreetingContainer />
     </header>
-      <body>
+    <span className="intro-span">
+    <div className="intro-greeting">
+      <h1>Discover your next favorite thing</h1>
+      <h6>Pinpoint surfaces the best new products, every day.
+        Its a place for product-loving enthusiasts to share and
+        geek out about the latest mobile apps, websites,
+        hardware projects, and tech creations</h6>
+      <Link className="intro-signup" to="/signup">SIGN UP</Link>
+    </div>
+    <div className="kitty">
+      <img src="https://s3.producthunt.com/static/kitty_265x244%402x.png"/>
+    </div>
+    </span>
       <Switch>
         <AuthRoute path="/login" component={SessionFormContainer} />
         <AuthRoute path="/signup" component={SessionFormContainer} />
       </Switch>
-      </body>
+
   </div>
 );
 
