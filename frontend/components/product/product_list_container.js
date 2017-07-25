a8514fb1ee09b1429871a9bc2be4421200197d55
inkymaze <nickwcannon@gmail.com>
2017-07-24 17:15:12 -0700

    CSS style products index

--- frontend/components/product/product_list_container.js
+++ frontend/components/product/product_list_container.js
@@ -1,14 +1,15 @@
 import { connect } from 'react-redux';
 import ProductList from './product_list';
 // import { selectAllProducts } from '../../reducers/selectors';
-import {requestProducts} from '../../actions/product_actions';
+import {requestProducts, requestProduct } from '../../actions/product_actions';
 
 const mapStateToProps = ({ products }) => ({
   products
 });
 
 const mapDispatchToProps = dispatch => ({
-  requestProducts: () => dispatch(requestProducts())
+  requestProducts: () => dispatch(requestProducts()),
+  requestProduct: (id) => dispatch(requestProduct(id))
 });
 
 
--- frontend/components/product/product_list_item.jsx
+++ frontend/components/product/product_list_item.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ProductListItem = ({ products }) => (
-
-  <div className="products-list-item">
-      <div className="product-img">
-          <img src={products.image_url} alt={products.name} />
-      </div>
-      <div className="product-desc">
-        <ul>
-          <li className="product-name">{ products.name } </li>
-          <li className="products-desc">{ products.desc } </li>
-        </ul>
-      </div>
-
-  </div>
+  <Link to={`/products/${products.id}`}>
+    <div className="products-list-item">
+        <div className="product-img">
+            <img src={products.image_url} alt={products.name} />
+        </div>
+        <div className="product-desc">
+          <ul>
+            <li className="product-name">{ products.name } </li>
+            <li className="products-desc">{ products.desc } </li>
+          </ul>
+        </div>
+    </div>
+  </Link>
 );
 
 export default ProductListItem;
