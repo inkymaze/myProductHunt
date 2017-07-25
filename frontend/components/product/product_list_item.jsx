89dc3575ec080cb7fb517ff4389e915c59b0aac9
inkymaze <nickwcannon@gmail.com>
2017-07-25 15:05:50 -0700

    text style in CSS auth form

--- frontend/components/product/product_list_item.jsx
+++ frontend/components/product/product_list_item.jsx
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 const ProductListItem = ({ products }) => (
   <Link to={`/products/${products.id}`}>
     <div className="products-list-item">
-        <div className="product-img">
-            <img src={products.image_url} alt={products.name} />
-        </div>
-        <div className="product-desc">
-          <ul>
-            <li className="product-name">{ products.name } </li>
-            <li className="products-desc">{ products.desc } </li>
-          </ul>
-        </div>
-        
+      <div className="product-img">
+          <img src={products.image_url} alt={products.name} />
+      </div>
+      <div className="product-desc">
+        <ul>
+          <li className="product-name">{ products.name } </li>
+          <li className="products-desc">{ products.desc } </li>
+        </ul>
+      </div>
+
 
     </div>
   </Link>
