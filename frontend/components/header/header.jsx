7480f57910c77c36b1cf5e0a48132e2e98a2f704
inkymaze <nickwcannon@gmail.com>
2017-07-26 09:09:57 -0700

    new url for logo

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -5,24 +5,24 @@ import { Link } from 'react-router-dom';
 
 const Header = (props) => (
   <div className="homepageHeader">
-    <div className='logoWithTitle'>
-      <div className="header-link">
-        <Link to="/" >
-          <img className='header-logo' src=
-            "http://res.cloudinary.com/dbyoymbpd/image/upload/c_scale,w_165/v1500499099/logo_pjzgg0.jpg"  />
-        </Link>
+      <div className='logoWithTitle'>
+        <div className="header-link">
+          <Link to="/" >
+            <img className='header-logo' src=
+              "http://res.cloudinary.com/dbyoymbpd/image/upload/c_scale,w_32/v1500499099/logo_pjzgg0.jpg"  />
+          </Link>
+        </div>
+        <div className="header-title">
+          <h2 className="header-title">Product Hunt</h2>
+          <h3 className="header-desc">The best new products, every day</h3>
+        </div>
       </div>
-      <div className="header-title">
-        <h2 className="header-title">Product Hunt</h2>
-        <h3 className="header-desc">The best new products, every day</h3>
+      <div>
+        <input type="text"  className="search-bar" placeholder="Discover your next favorite thing..."></input>
       </div>
+      <div>
+        <GreetingContainer />
       </div>
-    <div>
-      <input type="text"  className="search-bar" placeholder="Discover your next favorite thing..."></input>
-    </div>
-    <div>
-      <GreetingContainer />
-    </div>
 
   </div>
 
