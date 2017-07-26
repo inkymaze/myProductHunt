7f74a85d2f12dbf0dcf30e4164fb96ecb653cafa
inkymaze <nickwcannon@gmail.com>
2017-07-26 10:35:45 -0700

    add user jbuilder views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
-    resource :user, only: [:create]
+    resource :user, only: [:create, :show, :update]
     resources :products, only: [:create, :show, :index]
     resource :session, only: [:create, :destroy, :show]
   end
