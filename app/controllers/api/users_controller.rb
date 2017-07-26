5b21838058ce132a3b492725d544e1fa3e462c6e
inkymaze <nickwcannon@gmail.com>
2017-07-26 10:31:17 -0700

    add update and show to users cont

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,11 +11,34 @@ class Api::UsersController < ApplicationController
     end
   end
 
+    def show
+     @user = User.find_by_id(params[:id])
+
+     if @user
+       render '/api/users/show'
+     else
+       render( json:["User not found"], status: 404 )
+     end
+   end
+
+   def update
+    @user = User.find_by_id(params[:id])
+
+    if @user.username == current_user.username
+      if @user.update_attributes(user_params)
+        render 'api/users/show'
+      else
+        render json: @user.errors.full_messages, status: 422
+      end
+    else
+      render( json:["Update Failed"], status: 403 )
+    end
+  end
 
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :image_url)
   end
 end
