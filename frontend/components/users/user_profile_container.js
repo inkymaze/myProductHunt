510c48a500da0547526966e23fdd6c61c2933d4b
inkymaze <nickwcannon@gmail.com>
2017-10-13 10:17:08 -0700

    pass on products to user profile

--- frontend/components/users/user_profile_container.js
+++ frontend/components/users/user_profile_container.js
@@ -4,25 +4,24 @@ import { requestSingleUser,
   requestUpdateUser } from '../../actions/user_actions';
 import { requestUserProducts } from '../../actions/product_actions';
 import { requestUpvote } from '../../actions/upvote_actions';
-import { requestProduct } from '../../actions/product_actions';
 
 
 const mapStateToProps = (state, ownProps) => ({
   user:  state.users.byId[ownProps.match.params.userId],
   userId: ownProps.match.params.userId,
-  products: state.products,
   loggedIn: Boolean(state.session.currentUser),
+  products: state.products,
   currentUserId: state.session.currentUser
                 ? state.session.currentUser.id
                 : null,
   upvotedByUser:(state.session.currentUser)
                 ? state.session.currentUser.upvoted_products_ids
-                : null
+                : null,
 });
 
 const mapDispatchToProps = dispatch => ({
   requestSingleUser: (id) => dispatch(requestSingleUser(id)),
-  requestProduct: (id) => dispatch(requestProduct(id)),
+  requestUpdateUser: (user) => dispatch(requestUpdateUser(user)),
   requestUserProducts: userId => dispatch(requestUserProducts(userId)),
   requestUpvote: (upvote) => dispatch(requestUpvote(upvote))
 });
