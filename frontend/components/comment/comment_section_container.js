045a80d62f6e0745e58d1bad2418722e52b943f0
inkymaze <nickwcannon@gmail.com>
2017-10-13 12:54:55 -0700

    fix comment component structure

---
+++ frontend/components/comment/comment_section_container.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import { requestComments,
+         requestCreateComment } from '../../actions/comment_actions';
+import CommentSection from './comment_section';
+
+const mapStateToProps = (state) => ({
+  comments: state.comments.byId,
+  currentUserId: state.session.currentUser
+                  ? state.session.currentUser.id
+                  : null,
+  errors: state.comments.errors
+});
+
+const mapDispatchToProps = dispatch => ({
+  requestComments: (productId) => dispatch(requestComments(productId)),
+  requestCreateComment: (comment) => dispatch(requestCreateComment(comment)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CommentSection);
