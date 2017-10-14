2f6150ee772fbacfc9a39c346ced880432ff84ea
inkymaze <nickwcannon@gmail.com>
2017-10-13 18:36:00 -0700

    add comment item lifecycle method and handle create

--- frontend/components/comment/comment_list_item.jsx
+++ frontend/components/comment/comment_list_item.jsx
@@ -4,7 +4,23 @@ import React from 'react';
 class CommentListItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { body: ""};
+    this.createComment = this.createComment.bind(this);
+  }
 
+  componentDidMount() {
+    this.props.requestComments(this.props.productId);
+  }
+
+  createComment(e) {
+    e.preventDefault();
+    const newComment = {
+      "body" : this.state.body,
+      "hunter_id" : this.props.currentUserId,
+      "product_id" : this.props.productId
+    };
+    this.props.requestCreateComment({newComment})
+      .then(() => this.setState({body: ""}));
   }
 
   render() {
