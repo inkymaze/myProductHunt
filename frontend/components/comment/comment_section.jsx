62d2df1ef92930a406fc8baafa3752ede3788b49
inkymaze <nickwcannon@gmail.com>
2017-10-15 16:30:50 -0700

    add validation to add comments

--- frontend/components/comment/comment_section.jsx
+++ frontend/components/comment/comment_section.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import CommentListItem from './comment_list_item';
 import CommentForm from './comment_form';
 import _ from 'lodash';
-
+import { Link } from 'react-router-dom';
 
 class CommentSection extends React.Component {
   constructor(props) {
     super(props);
     this.renderComments = this.renderComments.bind(this);
+    this.renderCommentForm = this.renderCommentForm.bind(this);
+    this.renderSignupPrompt = this.renderSignupPrompt.bind(this);
   }
 
 componentDidMount() {
@@ -26,16 +28,39 @@ renderComments() {
   return comments;
 }
 
+renderCommentForm(){
+  return (
+    <CommentForm
+      currentUserImg={this.props.currentUserImg}
+      productId={this.props.productId}
+      currentUserId={this.props.currentUserId}
+      requestCreateComment={this.props.requestCreateComment}/>
+  );
+}
+
+renderSignupPrompt() {
+  return (
+    <span className='signup-prompt-outer'>
+      You need to become a Contributor to join the discussion -
+      <Link to="/signup"
+            className="signup-prompt-link"> Find out how</Link>
+      .
+    </span>
+  );
+}
+
   render () {
+    let commentForm;
+    if (this.props.currentUserId) {
+      commentForm = this.renderCommentForm();
+    } else {
+      commentForm = this.renderSignupPrompt();
+    }
     return (
       <div>
         <div className="comments-header">DISCUSSION</div>
         <div className="comment-section">
-          <CommentForm
-            currentUserImg={this.props.currentUserImg}
-            productId={this.props.productId}
-            currentUserId={this.props.currentUserId}
-            requestCreateComment={this.props.requestCreateComment}/>
+              {commentForm}
             <ul className='commens-list'>
               {this.renderComments()}
             </ul>
