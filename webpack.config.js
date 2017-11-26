8a378311d0207ef83c76a543358dd654397b39aa
inkymaze <nickwcannon@gmail.com>
2017-11-25 18:07:04 -0800

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,53 @@
+// var path = require("path");
+// var webpack = require("webpack");
+//
+// var plugins = [];
+// var devPlugins = [];
+//
+// // var prodPlugins = [
+// //   new webpack.DefinePlugin({
+// //     'process.env': {
+// //       'NODE_ENV': JSON.stringify('production')
+// //     }
+// //   }),
+// //   new webpack.optimize.UglifyJsPlugin({
+// //     compress: {
+// //       warnings: true
+// //     }
+// //   })
+// // ];
+//
+// plugins = plugins.concat(
+//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+// );
+//
+// module.exports = {
+//   context: __dirname,
+//   entry: "./frontend/index.jsx",
+//   output: {
+//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+//     filename: "bundle.js"
+//   },
+//   plugins: plugins,
+//   module: {
+//     loaders: [
+//       {
+//         test: [/\.jsx?$/, /\.js?$/],
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['es2015', 'react']
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-maps',
+//   resolve: {
+//     extensions: [".js", ".jsx", "*"]
+//   }
+// };
+
 var path = require("path");
-var webpack = require("webpack");
-
-var plugins = [];
-var devPlugins = [];
-
-var prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
 
 module.exports = {
   context: __dirname,
@@ -28,7 +56,6 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
-  plugins: plugins,
   module: {
     loaders: [
       {
